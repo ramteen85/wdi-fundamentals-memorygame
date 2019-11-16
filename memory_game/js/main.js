ab012d8d3269ada6f908c2156febcd1fa1d25ac1
ramteen <ram@wisdom.com.au>
2019-11-16 14:16:46 +1100

    final changes

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,22 +22,44 @@ var cards = [
 ];
 var cardsInPlay = [];
 
-function checkForMatch() {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
-    } else {
-        console.log("Sorry, try again.");
+function checkForMatch(clickedCard) {
+    console.log("clicked " + clickedCard);
+    var found = false;
+    console.log(cardsInPlay[0]);
+    for(var x = 0; x < cardsInPlay.length; x++) {
+        if(cardsInPlay[x] === clickedCard) {
+            found = true;
+            alert("You found a match!");
+        }
     }
+
+    if(cardsInPlay.length > 0 && !found)
+    {
+        alert("Sorry, try again.");
+    }
+    
 }
 
-function flipCard(cardId) {
+function flipCard() {
+    var cardId = this.getAttribute('data-id');
+    var clickedCard = document.querySelectorAll('#game-board img');
+    clickedCard[cardId].src = cards[cardId].cardImage;
     console.log("User flipped " + cards[cardId].rank);
+    checkForMatch(cards[cardId].rank);
     cardsInPlay.push(cards[cardId].rank);
     console.log("Image path: " + cards[cardId].cardImage);
     console.log("Card suit: " + cards[cardId].suit);
 }
 
+function createBoard() {
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        var gameBoard = document.querySelector('#game-board');
+        gameBoard.appendChild(cardElement);
+    }    
+}
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+createBoard();
